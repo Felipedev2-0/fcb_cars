001d8c6bd1ac9798a820948bf0d0a9328b6bef83
Felipe Barros <filbarros@hotmail.com>
2024-06-27 09:00:53 -0300

    fcb cars

---
+++ script.js
@@ -0,0 +1,6 @@
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let items = container.querySelectorAll('.list .item')
+let indicator = document.querySelector('.indicators')
+let dots = indicator.querySelectorAll('ul li')
